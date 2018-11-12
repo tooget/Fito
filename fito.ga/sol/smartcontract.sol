131dd03f7fcab27902852b46bf56cd85217221c0
awesomemix <Awesomemix1207@gmail.com>
2018-11-12 11:53:39 +0900

    일부 수정(Token 주식 관련 업데이트X)

--- fito.ga/sol/smartcontract.sol
+++ fito.ga/sol/smartcontract.sol
@@ -1,12 +1,14 @@
 pragma solidity ^0.4.25;
 
-contract GeneralMeeting{
+//token 초기값 이슈 아직X
 
+contract GeneralMeeting{
   uint totalTrades; //거래횟수
   uint totalShareholder//주주수
-  address contractCaller; //거래호출자
-  
+  address contractCaller; //거래호출;
+
   struct stockData{
+    uint idx;
     address walletAddr; //주주
     uint transactionHash;
     uint timestamp;
@@ -18,14 +20,16 @@ contract GeneralMeeting{
     contractCaller = msg.sender; //송신자(함수 주체)
   }
 
-  function addStocks(address _walletAddr){
-    stockData.push(stockData(_walletAddr, tx.hash , now));
+  //주식 거래
+  function addStocks(address _walletAddr){ //token 연동
+    stockData.push(stockData(totalTrades, _walletAddr, tx.hash, now));
     totalTrades++;
   }
 
   //거래 내역 리턴
-  function getProductStruct(address _walletAdd) public view returns (address,){
-    
+  function getProductStruct(uint _idx, address _walletAddr) public view returns (address, uint){
+    if(stockData[_idx].walletAddr == _walletAddr){
+      return(stockData[_idx].walletAddr, stockData[_idx].timestamp);
+    }
   }
-  
 }
