d7b1e2decaef2fb8b3d8b9dc2474d750cc4aba27
awesomemix <Awesomemix1207@gmail.com>
2018-11-12 12:25:20 +0900

    Update smartcontract.sol

--- fito.ga/sol/smartcontract.sol
+++ fito.ga/sol/smartcontract.sol
@@ -3,26 +3,33 @@ pragma solidity ^0.4.25;
 //token 초기값 이슈 아직X
 
 contract GeneralMeeting{
+  uint companyToken = 10000; //회사-주주
+  address companyAddr = 0;
   uint totalTrades; //거래횟수
   uint totalShareholder//주주수
-  address contractCaller; //거래호출;
+  //address contractCaller; //거래호출;
 
   struct stockData{
     uint idx;
-    address walletAddr; //주주
-    uint transactionHash;
+    uint token; //
+    address walletAddrFrom; //주주
+    address walletAddrTo; //주주
+    /* uint transactionHash; */   //tx.hash
+    uint tradingPrice;
     uint timestamp;
   }
 
   stockData[] public trades;
 
   constructor() public{
-    contractCaller = msg.sender; //송신자(함수 주체)
+    //contractCaller = msg.sender; //송신자(함수 주체)
   }
 
-  //주식 거래
-  function addStocks(address _walletAddr){ //token 연동
-    stockData.push(stockData(totalTrades, _walletAddr, tx.hash, now));
+  //stockData 추가 ==> 거래 =
+  function addStocks(address _walletAddrTo, uint _tradingPrice bool isCompany){ //token 연동
+    //false = 주주-주주 true = 회사-주주
+    if(isCompany == false) stockData.push(stockData(totalTrades, aaa, msg.sender, _walletAddrTo, _tradingPrice, wnow));
+    else stockData.push(stockData(totalTrades, , msg.sender, _walletAddrTo, _tradingPrice, wnow));
     totalTrades++;
   }
 
