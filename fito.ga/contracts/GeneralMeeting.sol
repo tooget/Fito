7acd396d50dd65f2b29423065a7e2d64c1684561
tooget <tooget@gmail.com>
2018-11-13 16:08:09 +0900

    GeneralGreetings.sol Test Success in Ganache.

--- fito.ga/contracts/GeneralMeeting.sol
+++ fito.ga/contracts/GeneralMeeting.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.4.24;
 //token 초기값 이슈 아직X
 
 contract GeneralMeeting {
+
     uint shareOfCompany = 10000; //회사-주주
     address ethTaker;
+    uint balance;
 
     mapping (address => uint) ownedShares;
 
@@ -20,38 +22,65 @@ contract GeneralMeeting {
     tradingData[] public tradingDatas;
 
     function initShares(address _corpAddr) public {
-        ownedShares[_corpAddr] = shareOfCompany;
+        address corpAddr = _corpAddr;
+        ownedShares[corpAddr] = shareOfCompany;
         shareOfCompany = 0;
     }
 
+    // transfer를 위한 
+    //로깅을 위한 이벤트
+    event Sended(
+        uint256 _value,
+        uint256 _balance
+    );
+
+    // 
     function setEthTaker(address _ethTaker) private {
         ethTaker = _ethTaker;
+    }
+
+    //호출자가 CA에게 이더를 전송
+    function sending() payable public {
+        balance = msg.value;
+        emit Sended(msg.value, balance);
+    }
+
+    //set함수를 통해 저장한 주소에 이더를 전송하는 함수
+    function transfer() payable public {
+        ethTaker.transfer(msg.value);
     }
 
     //stockData 추가 ==> 양수자가 거래 요청
     function addTradingData(address _sellerAddr, uint _tradingShares) payable public returns(uint) {
         require(ownedShares[_sellerAddr] >= _tradingShares && _tradingShares != 0);
-        tradingData memory newTradingData = tradingData(_tradingShares, msg.sender, _sellerAddr, msg.value, now, false);
+        tradingData memory newTradingData = tradingData(_tradingShares, _sellerAddr, msg.sender, msg.value, now, false);
         uint tradingIdx = tradingDatas.push(newTradingData);
         setEthTaker(_sellerAddr);
-        ownedShares[msg.sender] -= _tradingShares;
-        ownedShares[_sellerAddr] += _tradingShares;
-        _sellerAddr.transfer(msg.value);
-        tradingDatas[tradingIdx].isTradingComplete = true;
-        return tradingIdx;
+        sending();
+        ownedShares[_sellerAddr] -= _tradingShares;
+        ownedShares[msg.sender] += _tradingShares;
+        transfer();
+        tradingDatas[tradingIdx-1].isTradingComplete = true;
+        return tradingIdx-1;
     }
 
-    //총 거래 내역 수 리턴
-    // function getLengthOftradingDatas() public view returns (uint) {
-    //     return tradingDatas.length();
-    // }
+    // 총 거래 내역 수 리턴
+    function getLengthOftradingDatas() public view returns (uint) {
+        return tradingDatas();
+    }
 
     //거래 내역 리턴
     // function getProductStruct(uint _idx) public view returns (tradingData[]) {
     //     return tradingDatas[_idx];
     // }
 
-    function getTotalStuck(address _ownerAddr) public view returns (uint){
+    function getOwnedShares(address _ownerAddr) public view returns (uint){
         return ownedShares[_ownerAddr];
     }
+
+    function getShareOfCompany() public view returns(uint) {
+        return shareOfCompany;
+    }
+
+    function sendEth() public payable {}
 }
