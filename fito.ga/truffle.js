834a4643b5fe2ebb5f20e638b841212d9e61864f
tooget <tooget@gmail.com>
2018-11-16 04:05:47 +0900

    dApp FitoContract.sol, test success in Ganache.

--- fito.ga/truffle.js
+++ fito.ga/truffle.js
@@ -1,14 +1,9 @@
-var Web3 = require('web3');
-
 module.exports = {
-networks: {
-    dapp_fito_ga: {
-      provider: () => {
-        return new Web3.providers.HttpProvider('https://k0k0j8s4h0:0UhNW5Fl82OSkrx-S2-nmgqg8kdSMVhgOuRrZu-pCX0@k0idprbzja-k0dsk7yusm-rpc.ap-northeast-2.kaleido.io');
-      },
-      network_id: "*",
-      gasPrice: 0,
-      gas: 4500000
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545,     // Ganache
+      network_id: "*" // Match any network id
     }
   }
 };
